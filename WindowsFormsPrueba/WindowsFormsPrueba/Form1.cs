b12bc486b214732e2f71337b17e30beef7f20426
Areli <lic.areli.ortega@gmail.com>
2017-09-07 10:31:31 -0600

    windows forms c#

---
+++ WindowsFormsPrueba/WindowsFormsPrueba/Form1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsPrueba
+{
+    public partial class Form1 : Form{
+
+        String cadena1;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            cadena1 = textBox1.Text;//obtiene el texto del text box
+            textBox2.Text = cadena1;//setea el texto
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        
+
+        
+    }
+}
